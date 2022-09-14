bca1943031bf647ab9fa5c0470fbf4fec4eee04e
mykolabmv <gst.bnv@gmail.com>
2022-09-14 18:19:31 +0300

    fix 1

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm blocked*.conf
 
-cat domains.nkrz|uniq| while read hosts
+cat domains.nkrz|sort -u| while read hosts
 do
 	echo "zone $hosts {type master; file \"/etc/bind/blocked/block\"; };" >> blocked_linux.conf
         echo "zone $hosts {type master; file \"/etc/namedb/blocked/block\"; };" >> blocked_bsd.conf
